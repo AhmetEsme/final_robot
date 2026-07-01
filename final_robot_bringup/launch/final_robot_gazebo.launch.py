70f2b8a1ce594e8fba9ff8401345607261928287
AhmetEsme <280829604+AhmetEsme@users.noreply.github.com>
2026-07-02 08:29:28 +0900

    Add collision, inertial, Gazebo material, Gazebo launch - Ders 58 complete

---
+++ final_robot_bringup/launch/final_robot_gazebo.launch.py
@@ -0,0 +1,41 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+
+    urdf_path = os.path.join(
+        get_package_share_directory('final_robot_description'),
+        'urdf',
+        'standalone_arm.urdf.xacro'
+    )
+
+    gazebo_launch_path = os.path.join(
+        get_package_share_directory('ros_gz_sim'),
+        'launch',
+        'gz_sim.launch.py'
+    )
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[
+                {'robot_description': Command(f'xacro {urdf_path}')},
+                {'use_sim_time': True}
+            ]
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(gazebo_launch_path),
+            launch_arguments={'gz_args': '-v 4 -r empty.sdf'}.items()
+        ),
+        Node(
+            package='ros_gz_sim',
+            executable='create',
+            arguments=['-topic', 'robot_description', '-name', 'final_robot']
+        )
+    ])
