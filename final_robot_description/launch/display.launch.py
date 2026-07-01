d0f56f6b69af39140fa4b8608ff70af87acd29fa
AhmetEsme <280829604+AhmetEsme@users.noreply.github.com>
2026-07-01 10:52:54 +0900

    Add display.launch.py without rviz config

---
+++ final_robot_description/launch/display.launch.py
@@ -0,0 +1,30 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command
+
+def generate_launch_description():
+
+    urdf_path = os.path.join(
+        get_package_share_directory('final_robot_description'),
+        'urdf',
+        'standalone_arm.urdf.xacro'
+    )
+
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': Command(f'xacro {urdf_path}')}]
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+        )
+    ])
